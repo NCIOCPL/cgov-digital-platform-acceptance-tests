5773fdda43e5da4cce299dbe3aee346b55a43033
kate-mashkina <mashkinaea@nci-02181235-ml.myfiosgateway.com>
2020-07-23 11:37:56 -0400

    (#30) changed the way prop29 is validated

--- cypress/integration/common/analytics.js
+++ cypress/integration/common/analytics.js
@@ -4,6 +4,26 @@ import { getClickBeacon } from "../../utils/getClickBeacon.js"
 import { getLoadBeacon } from "../../utils/getLoadBeacon.js"
 
 let loadOrClick;
+//date, months and weekDays are used to generate date and time value for prop29
+const date = new Date();
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+];
+
+const weekDays = [
+    'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday',
+]
 
 Then('the following parameters should be captured', dataTable => {
     let beacon = getBeacon();
@@ -39,6 +59,35 @@ Then('the following parameters should be captured', dataTable => {
             } else
                 expect(mapOfEvents.get(parameter)).to.be.eq(value);
 
+        } else if (parameter === 'prop29') {
+            const year = date.getFullYear();
+            const month = months[date.getMonth()];
+            const day = date.getDate();
+            const weekDay = weekDays[date.getDay()];
+            const hours24 = date.getHours();
+            let dayMode = hours24 < 12 ? 'AM' : 'PM';
+            const hours12 = hours24 <= 12 ? (hours24 === 0 ? 12 : hours24) : hours24 - 12;
+            const minutes = (date.getMinutes() < 10) ? `0${date.getMinutes()}` : date.getMinutes();
+            // in case test will be run right when the clock turns extra minute,
+            //so that beacon will record a value of 16 mins, but date executes before the beacon,
+            //it will be 15 minutes
+            //we will account for that minute by adding one to currently captured time
+            let extraMin = (typeof minutes === 'string' && minutes !== '09') ? `0${parseInt(minutes) + 1}` :
+                (minutes === '09') ? '10' :
+                    minutes + 1;
+            // also there is a case when test is run when the new hour starts (01:59)
+            //so we need to add an option when the time might be 02:00 vs 01:59
+            //plus the case when it's 11:59 AM turning into 12:00 PM
+            let extraHour = hours12;
+            if (extraMin === 60) {
+                extraHour += 1;
+                extraMin = '00';
+                if (extraHour === 12) {
+                    dayMode = dayMode === 'AM' ? 'PM' : 'AM';
+                }
+            }
+            const finalString = new RegExp(`(year=${year} \| month=${month} \| date=${day} \| day=${weekDay} \| time=)(${hours12}|${extraHour}):(${minutes}|${extraMin}) ${dayMode}`);
+            expect(beacon[parameter]).to.match(finalString);
         } else
             expect(beacon[parameter]).to.be.equal(value);
     }
