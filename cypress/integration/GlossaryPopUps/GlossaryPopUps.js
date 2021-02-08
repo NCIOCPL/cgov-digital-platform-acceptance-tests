0c6da758bb263310788ffd8b2a9a8b6cda33beaf
alinai <alinai@users.noreply.github.com>
2021-02-08 13:10:58 -0500

    (#66) Added tests for Glossary pop-ups

---
+++ cypress/integration/GlossaryPopUps/GlossaryPopUps.js
@@ -0,0 +1,50 @@
+// <reference types="Cypress" />
+import { Given, And, Then } from 'cypress-cucumber-preprocessor/steps';
+
+Given('user is navigating to {string}', (a) => {
+    Cypress.on('uncaught:exception', (err, runnable) => {
+        // returning false here to Cypress from
+        // failing the test
+        return false;
+    });
+    cy.visit(a);
+});
+
+When('user is clicking on definition for {string}', (termName) => {
+    cy.get('a.definition').contains(termName).click();
+    cy.wait(500);
+});
+
+Then('pop-up definition box is displayed for {string}', (termName) => {
+    cy.get('div.modal__container .term').should('include.text', termName);
+});
+
+And('definition box displays all {string} provided by glossary', (features) => {
+    const expText = features.split(',');
+    for (let i = 0; i < expText.length; i++) {
+        switch (expText[i]) {
+            case 'definition':
+                cy.get('div.modal__container dd[class="definition"]').should('be.visible');
+                break;
+            case 'pronunciation':
+                cy.get('div.modal__container span[class="pronunciation"]').should('be.visible');
+                break;
+            case 'audio':
+                cy.get('div.modal__container a[class="CDR_audiofile"]').should('be.visible');
+                break;
+            default:
+                cy.get('div.modal__container').find('img').as('image');
+                cy.get('@image').should('have.attr', 'src');
+                cy.get('@image').should('have.attr', 'alt');
+                const count = parseInt(((expText[i].replace('img[', '')).replace(']', '')));
+                cy.get('@image').should('have.length', count);
+                break;
+        }
+    }
+});
+
+
+
+
+
+
