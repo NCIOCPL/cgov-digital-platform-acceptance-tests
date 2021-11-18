daec82952e7e32b2bd1cc5cbe88cdb20254867f1
alinai <alinai@users.noreply.github.com>
2021-11-18 15:06:52 -0500

    (#121) Search Results Page tests - disabled chromeSecurity to alow cross origin testing -added tests

---
+++ cypress/integration/CTS/SearchResultsPageTests.feature
@@ -0,0 +1,80 @@
+Feature: As an user, I should be able to see my search results displayed, select and print them
+
+    Scenario: Search results page displays general page components
+        Given screen breakpoint is set to "desktop"
+        Given user is navigating to "/about-cancer/treatment/clinical-trials/search/r?loc=0&q=breast%20cancer&rl=2"
+        Then page title is "Clinical Trials Search Results"
+        And trial info displays "Results 1-10  of \d+ for your search.*"
+        And "Show Search Criteria" button is displayed
+        When user clicks on "Show Search Criteria" button
+        Then "Hide Search Criteria" button is displayed
+        And search criteria table displays the following
+            | Category         | Selection     |
+            | Keywords/Phrases | breast cancer |
+        And "Start Over" link has a href "/about-cancer/treatment/clinical-trials/search/advanced"
+        And "Modify Search Criteria" button is displayed
+        And "top" pager is displayed
+        And "bottom" pager is displayed
+        And result list is displayed
+        And each result item is a link
+        #There is a ticket for implementing the workaround for the commented step below
+        #(https://github.com/NCIOCPL/cgov-digital-platform-acceptance-tests/issues/137)
+        # And there are 2 delighters present
+        And the following delighters are displayed
+            | delighter    | href                                                       | title                              | text                                                                  |
+            | cts-livehelp | /contact                                                   | Have a question?We're here to help | Chat with us: LiveHelpCall us: 1-800-4-CANCER(1-800-422-6237)         |
+            | cts-which    | /about-cancer/treatment/clinical-trials/search/trial-guide | Which trials are right for you?    | Use the checklist in our guide to gather the information you’ll need. |
+
+    Scenario: Clicking on chat now will fire an analytics event
+        Given screen breakpoint is set to "desktop"
+        Given user is navigating to "/about-cancer/treatment/clinical-trials/search/r?loc=0&rl=1"
+        Then page title is "Clinical Trials Search Results"
+        And chat pop up is displayed
+        When user clicks on chat button
+        Then page click request is sent
+        And the following parameters should be captured
+            | parameter | value                                                            |
+            | event44   |                                                                  |
+            | link      | Chat Now                                                         |
+            | linkType  | lnk_o                                                            |
+            | prop67    | D=pageName                                                       |
+            | prop4     | D=pev1                                                           |
+            | prop8     | english                                                          |
+            | prop5     | livehelp_proactive chat - launch\|D=pageName                     |
+            | pageName  | {CANONICAL_HOST}/about-cancer/treatment/clinical-trials/search/r |
+            | evar2     | english                                                          |
+
+
+    Scenario: User is able to select results on mobile breakpoint
+        Given screen breakpoint is set to "mobile"
+        When user is navigating to "/about-cancer/treatment/clinical-trials/search/r?loc=0&q=breast%20cancer&rl=2"
+        Then page title is "Clinical Trials Search Results"
+        And all result item's checkboxes are not checked
+        And "Select all on page" checkbox is displayed
+        When user selects "Select all on page" checkbox
+        Then all result item's checkboxes are checked
+        When user selects 1 result's checkbox
+        Then "Select all on page" checkbox is not checked
+        And 1 result's checkbox is not checked
+        When user selects "Select all on page" checkbox
+        Then "Select all on page" checkbox is checked
+        And 1 result's checkbox is checked
+        When user selects "Select all on page" checkbox
+        Then all result item's checkboxes are not checked
+
+    Scenario: User is able to print results
+        Given screen breakpoint is set to "tablet"
+        Given user is navigating to "/about-cancer/treatment/clinical-trials/search/r?loc=0&q=breast%20cancer&rl=2"
+        Then page title is "Clinical Trials Search Results"
+        When user selects 1 result's checkbox
+        And user clicks on "Print Selected" button at "top" position
+        Then user is redirected to "/CTS.Print/Display" with generated "printid"
+        And page title is "Your Clinical Trials Search Results"
+        And print page search criteria table displays the following
+            | Category          | Selection     |
+            | Keywords/Phrases: | breast cancer |
+        And "New Search" link has a href "/about-cancer/treatment/clinical-trials/advanced-search"
+        And "Print" link has a href "javascript:window.print();"
+        And "E-Mail These Results" link has a "target" attribute with "_blank" value
+        And "Check for Updates" link has a href that matches "\/about-cancer\/treatment\/clinical-trials\/search\/v\?q=breast\+cancer&loc=0&rl=2&id=NCI-\d+-\d+"
+
---
+++ cypress/integration/CTS/SearchResultsPageTests/index.js
@@ -0,0 +1,160 @@
+// <reference types="Cypress" />
+import { Given, And, Then, When } from 'cypress-cucumber-preprocessor/steps';
+
+And('trial info displays {string}', (infoText) => {
+    cy.get('.all-trials').invoke('text').then((text) => {
+        const resultsText = text.split('\n').join(' ').split('\t').join('');
+        const regex = new RegExp(infoText);
+        expect(resultsText).to.match(regex);
+    });
+});
+
+And('{string} button is displayed', (expectedButton) => {
+    cy.get('.cts-results-header button').should('be.visible').and('contain.text', expectedButton);
+});
+
+
+When('user clicks on {string} button', (searchCriteriaButton) => {
+    cy.get('button.cts-accordion__button').contains(searchCriteriaButton).click();
+});
+
+And('search criteria table displays the following', (dataTable) => {
+    cy.get('tbody > tr').should('have.length', dataTable.hashes().length);
+    //index assures correct order
+    let index = 0;
+    for (const { Category, Selection } of dataTable.hashes()) {
+        cy.get('tbody tr th').eq(index).should('have.text', Category);
+        cy.get('tbody tr td').eq(index).should('have.text', Selection);
+        index++;
+    }
+});
+
+And('{string} link has a href {string}', (linkText, linkHref) => {
+    cy.get('a').contains(linkText).should('have.attr', 'href').and('eq', linkHref);
+});
+
+And('{string} pager is displayed', (pagerPosition) => {
+    cy.get(`div.results-page__control.--${pagerPosition} .results-page__pager`).should('be.visible');
+});
+
+And('result list is displayed', () => {
+    cy.get('div.results-list').should('be.visible');
+});
+
+And('each result item is a link', () => {
+    cy.document().then((doc) => {
+        const titleLocator = doc.querySelectorAll('.results-list-item__title');
+        for (let i = 0; i < titleLocator.length - 1; i++) {
+            cy.get(titleLocator[i]).should('be.visible');
+            cy.get(titleLocator[i]).find('a').should('have.attr', 'href');
+        }
+    });
+});
+
+And('there are {int} delighters present', (count) => {
+    cy.get('.cts-delighter-container div[class^="delighter"]:visible').should('have.length', count);
+});
+
+And('the following delighters are displayed', (dataTable) => {
+    for (const { delighter, href, title, text } of dataTable.hashes()) {
+        cy.get(`div[class="delighter ${delighter}"]:visible`).as('delighter');
+        cy.get('@delighter').find('a').first().should('have.attr', 'href', href);
+        cy.get('@delighter').find('h4').first().should('have.text', title);
+        cy.get('@delighter').find('p').first().should('have.text', text);
+    }
+});
+
+And('chat pop up is displayed', () => {
+    cy.get('#ProactiveLiveHelpForCTSPrompt', { timeout: 30000 }).should('be.visible');
+});
+
+When('user clicks on chat button', () => {
+    cy.get('#chat-button').click();
+    cy.wait(2000);
+});
+
+And(`all result item's checkboxes are not checked`, () => {
+    cy.get('.results-list .cts-checkbox__label').each(($el) => {
+        const win = $el[0].ownerDocument.defaultView;
+        // use getComputedStyle to read the pseudo selector
+        const before = win.getComputedStyle($el[0], ':before');
+        // read the value of the `content` CSS property
+        const contentValue = before['background-image'];
+        // the returned value will have double quotes around it, but this is correct
+        expect(contentValue).to.eq('none');
+    });
+});
+
+And('{string} checkbox is displayed', (selectAllCheckbox) => {
+    cy.get(`label[for='select-all-cbx-top']`).should('be.visible').and('have.text', selectAllCheckbox);
+});
+
+When('user selects {string} checkbox', (selectAllCheckbox) => {
+    cy.get(`label[for='select-all-cbx-top']`).contains(selectAllCheckbox).click();
+});
+
+Then(`all result item's checkboxes are checked`, () => {
+    cy.get('.results-list .cts-checkbox__label').each(($el) => {
+        const win = $el[0].ownerDocument.defaultView;
+        // use getComputedStyle to read the pseudo selector
+        const before = win.getComputedStyle($el[0], '::before');
+        // read the value of the `content` CSS property
+        const contentValue = before['background-image'];
+        // the returned value will have double quotes around it, but this is correct
+        expect(contentValue).not.to.eq('none');
+    });
+});
+
+When(`user selects {int} result's checkbox`, (num) => {
+    cy.get(`.results-list .cts-checkbox__label`).eq(num - 1).click();
+});
+
+Then('{string} checkbox is not checked', (selectAllCheckbox) => {
+    cy.get('label').contains(selectAllCheckbox).parent().find('input').should('not.be.checked');
+});
+
+And(`{int} result's checkbox is not checked`, (num) => {
+    cy.get('.results-list .cts-checkbox__label').eq(num - 1).parent().find('input').should('not.be.checked');
+});
+
+Then('{string} checkbox is checked', (selectAllCheckbox) => {
+    cy.get('label').contains(selectAllCheckbox).parent().find('input').should('be.checked');
+});
+
+And(`{int} result's checkbox is checked`, (num) => {
+    cy.get('.results-list .cts-checkbox__label').eq(num - 1).parent().find('input').should('be.checked');
+});
+
+And('user clicks on {string} button at {string} position', (printButton, printButtonPosition) => {
+    cy.get(`div.results-page__control.--${printButtonPosition}`).find('button.results-page__print-button').contains(printButton).click();
+});
+
+Then('user is redirected to {string} with generated {string}', (redirectedPath, printID) => {
+    cy.location('pathname').should('eq', redirectedPath);
+    cy.location('search').should('include', `?${printID}=`);
+});
+
+And('print page search criteria table displays the following', (dataTable) => {
+    dataTable.rows().forEach((row) => {
+        for (let i = 0; i < row.length; i++) {
+            cy.get('.search-criteria-box tbody tr td').eq(i).should('have.text', row[i]);
+        }
+    });
+});
+
+And('{string} link has a {string} attribute with {string} value', (emailLink, attrTarget, attrValue) => {
+    cy.get(`a[target='_blank']`).as('target');
+    cy.get('@target').should('be.visible').and('contain.text', emailLink);
+    cy.get('@target').should('have.attr', attrTarget, attrValue);
+});
+
+And('{string} link has a href that matches {string}', (checkUpdateLink, path) => {
+    cy.get('.hangingIndent.print-check-for-updates').as('checkupdate');
+    cy.get('@checkupdate').should('be.visible').and('contain.text', checkUpdateLink);
+    const regex = new RegExp(path);
+    cy.get('@checkupdate').should('have.attr', 'href').and('match', regex);
+});
+
+
+
+
