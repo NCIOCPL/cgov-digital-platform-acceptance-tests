c92e0346d9ec3da88eba0de1ce5170fdac7bb63d
Saxena <saxenas3@nih.gov>
2021-11-23 16:10:20 -0500

    (#100) Simple Date Test

---
+++ cypress/integration/Crosscutting/SimpleDateTest/index.js
@@ -0,0 +1,23 @@
+// <reference types="Cypress" />
+import { Given, And, Then } from 'cypress-cucumber-preprocessor/steps';
+
+Then('date is displayed', () => {
+    cy.get('div.document-dates').should('be.visible');
+});
+
+And('the date {string} is {string}', (field, value) => {
+    cy.get('div.document-dates > ul >li').as('timestamp');
+    if (field == "text") {
+        cy.get('@timestamp').find('time').should('have.attr', 'datetime').and('eq', value);
+    }
+    else if (field == "label") {
+        cy.get('@timestamp').find('strong').should('include.text', value);
+    }
+    else if (field == "stamp") {
+        cy.get('@timestamp').find('time').should('include.text', value);
+    }
+});
+
+Then('dates do not appear', () => {
+    cy.get('div.document-dates').should('not.exist');
+});
