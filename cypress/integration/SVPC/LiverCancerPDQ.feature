79b55a92f130616d2b39b93690bd28852c49443a
Kate Mashkina <56886837+kate-mashkina@users.noreply.github.com>
2022-02-02 12:54:22 -0500

    (#143) SVPC Liver Cancer tests - added feature file - added step definitions

---
+++ cypress/integration/SVPC/LiverCancerPDQ.feature
@@ -0,0 +1,75 @@
+Feature: As a content owner I want to be able to split one summary into six different summaries so that each of the pages is more SEO-focused on its single topic
+
+    ###Optional on this page
+
+    Scenario Outline: User views the Liver Cancer Treatment content item showing On This Page on CGov on desktop and tablet
+        Given screen breakpoint is set to "<breakpoint>"
+        When user is navigating to "/test/types/liver/what-is-liver-cancer/treatment"
+        Then "On This Page" section displays below the intro text "This is an intro text. For more information, see Side Effects of Cancer Treatment."
+        And "On This Page" section displays before the first body section titled "Surveillance"
+        Examples:
+            | breakpoint |
+            | desktop    |
+            | tablet     |
+
+    Scenario: User views the Liver Cancer Treatment content item showing On This Page on CGov on mobile
+        Given screen breakpoint is set to "mobile"
+        When user is navigating to "/test/types/liver/what-is-liver-cancer/treatment"
+        Then the body section accordion displays below the intro text "This is an intro text. For more information, see Side Effects of Cancer Treatment."
+        And "On This Page" section does not display
+
+    Scenario Outline: User views the Liver Cancer Screening PDQ CIS content item hiding On This Page on CGov on desktop and tablet
+        Given screen breakpoint is set to "<breakpoint>"
+        When user is navigating to "/test/types/liver/what-is-liver-cancer/causes-risk-factors"
+        Then "On This Page" section does not exist
+        Examples:
+            | breakpoint |
+            | desktop    |
+            | tablet     |
+
+    Scenario: User views the Liver Cancer Screening PDQ CIS content item hiding On This Page on CGov on mobile
+        Given screen breakpoint is set to "mobile"
+        When user is navigating to "/test/types/liver/what-is-liver-cancer/causes-risk-factors"
+        Then "On This Page" section does not exist
+        And the body sections do not collapse into accordion
+
+    # #### Intro text
+
+    Scenario: User views the Liver Cancer Treatment content item with intro text on CGov on desktop
+        Given screen breakpoint is set to "desktop"
+        When user is navigating to "/test/types/liver/what-is-liver-cancer/treatment"
+        And the intro text displays "This is an intro text. For more information, see Side Effects of Cancer Treatment."
+        Then the intro text "This is an intro text. For more information, see Side Effects of Cancer Treatment." appears following the "h1" title "Liver Cancer Treatment"
+        And there is no anchor link to the intro text "#_47" from the On This Page
+
+    
+
+    Scenario: User views the Liver Cancer Treatment content item with intro text on CGov on tablet
+        Given screen breakpoint is set to "tablet"
+        When user is navigating to "/test/types/liver/what-is-liver-cancer/treatment"
+        And the intro text displays "This is an intro text. For more information, see Side Effects of Cancer Treatment."
+        And the intro text "This is an intro text. For more information, see Side Effects of Cancer Treatment." is displayed below page options
+        And there is no anchor link to the intro text "#_47" from the On This Page
+  
+
+    Scenario: User views the Liver Cancer Treatment content item with intro text on CGov on mobile
+        Given screen breakpoint is set to "mobile"
+        When user is navigating to "/test/types/liver/what-is-liver-cancer/treatment"
+        Then the intro text displays "This is an intro text. For more information, see Side Effects of Cancer Treatment."
+        And the intro text "This is an intro text. For more information, see Side Effects of Cancer Treatment." is displayed below page options
+        And the intro text "This is an intro text. For more information, see Side Effects of Cancer Treatment." is displayed above accordion
+
+      
+# ### Do Not Generate and Display "In This Section" on Cancer Information Summary content
+
+
+Scenario Outline: User views the Liver Cancer pdq cis item with and without In This Section content
+    Given user is navigating to "<url>"
+    Then In This Section section does not exist
+
+    Examples:
+        | url                                                   |
+        | /test/types/liver/what-is-liver-cancer/causes-risk-factors |
+        | /test/types/liver/what-is-liver-cancer/diagnosis           |
+        | /test/types/liver/what-is-liver-cancer/treatment           |
+        | /test/types/liver/what-is-liver-cancer                     |
---
+++ cypress/integration/SVPC/LiverCancerPDQ/index.js
@@ -0,0 +1,158 @@
+/// <reference types="Cypress" />
+import { And, Then } from "cypress-cucumber-preprocessor/steps";
+
+//This step is just a workaround until it gets fixed (see issue #118)
+Given("user is navigating to {string}", (a) => {
+  Cypress.on("uncaught:exception", (err, runnable) => {
+    //there is an existing issue with PDQ content console errors
+    // we don't want to fail the test so we return false
+    if (err.message.includes("getAttribute")) {
+      return false;
+    }
+  });
+  cy.visit(a);
+});
+
+Then(
+  "{string} section displays below the intro text {string}",
+  (onThisPage, introText) => {
+    cy.get("nav.on-this-page h6")
+      .should("be.visible")
+      .and("include.text", onThisPage);
+    cy.get("nav.on-this-page").prev().should("include.text", introText);
+  }
+);
+And(
+  "{string} section displays before the first body section titled {string}",
+  (onThisPage, title) => {
+    cy.get("nav.on-this-page h6").should("include.text", onThisPage);
+    cy.get("nav.on-this-page")
+      .next()
+      .find("section h2")
+      .first()
+      .invoke("text")
+      .should("include", title);
+  }
+);
+
+Then(
+  "the body section accordion displays below the intro text {string}",
+  (introText) => {
+    //verify that the on this page is not visible
+    cy.get('div[class^="accordion"]').prev().should("not.be.visible");
+    //verify intro text is directly above accordion and visible
+    cy.get('div[class^="accordion"]')
+      .prev()
+      .prev()
+      .should("be.visible")
+      .and("include.text", introText);
+  }
+);
+
+And("{string} section does not display", (onThisPage) => {
+  //verify that the on this page is not visible
+  cy.get('div[class^="accordion"]')
+    .prev()
+    .contains(onThisPage)
+    .should("not.be.visible");
+});
+
+Then("{string} section does not exist", (onThisPage) => {
+  cy.get("nav.on-this-page").should("not.exist");
+});
+
+And("the body section accordion displays below page options", () => {
+  cy.get('div[class^="accordion"]')
+    .parent()
+    .parent()
+    .prev()
+    .should("have.class", "page-options-container")
+    .and("be.visible");
+});
+
+And("the intro text displays {string}", (introText) => {
+  cy.get(".pdq-sections p").first().should("include.text", introText);
+});
+
+Then(
+  "the intro text {string} appears following the {string} title {string}",
+  (text, tag, title) => {
+    //find h1 first and assert its text
+    cy.get(".resize-content")
+      .children()
+      .first()
+      .as("pageTitle")
+      .should("have.text", title);
+    //title is followed by pageOption in the html tree and is not visible
+    cy.get("@pageTitle")
+      .next()
+      .as("pageOptions")
+      .should("have.class", "page-options-container")
+      .and("not.be.visible");
+    // pageOption is its turn is followed by intro text
+    cy.get("@pageOptions")
+      .next()
+      .find("p")
+      .first()
+      .should("be.visible")
+      .and("include.text", text);
+  }
+);
+
+And(
+  "there is no anchor link to the intro text {string} from the On This Page",
+  (anchorLink) => {
+    //get all anchor links from onThisPage and assert that none links to the intro text
+    cy.get("nav.on-this-page ul li a").should(
+      "not.have.attr",
+      "href",
+      anchorLink
+    );
+  }
+);
+
+And("the intro text {string} is displayed below page options", (text) => {
+  cy.get(".page-options-container").as("pageOptions").should("be.visible");
+  // pageOption is followed by intro text
+  cy.get("@pageOptions")
+    .next()
+    .as("summarySections")
+    .find("p")
+    .first()
+    .should("be.visible")
+    .and("include.text", text);
+});
+
+And("the intro text {string} is displayed above accordion", (text) => {
+  //intro text assert
+  cy.get(".summary-sections")
+    .children()
+    .first()
+    .as("introTextSection")
+    .find("p")
+    .first()
+    .as("introText")
+    .should("be.visible")
+    .and("include.text", text);
+  //intro text is followed by invisible 'on this page'
+  cy.get("@introTextSection")
+    .next()
+    .as("onThisPage")
+    .should("have.class", "on-this-page")
+    .and("not.be.visible");
+  // 'on this page' is followed by accordion, therefore accordion is directly preceeded by intro text (in terms of visibility)
+  cy.get("@onThisPage")
+    .next()
+    .should(($accordion) => {
+      const className = $accordion[0].className;
+      expect(className).to.include("accordion");
+    });
+});
+
+Then("In This Section section does not exist", () => {
+  cy.get("nav.in-this-section").should("not.exist");
+});
+
+And("the body sections do not collapse into accordion", () => {
+  cy.get('div[class^=accordion] h2').should('not.exist');
+});
