2845070ba21ce27221705491ec70a383ec75d476
alinai <alinai@users.noreply.github.com>
2022-02-11 12:56:18 -0500

    (#138) Updated Sprites Test Code

--- cypress/integration/AnnualPlan/ContentBlockAPButton/contentBlockButton.js
+++ cypress/integration/AnnualPlan/ContentBlockAPButton/contentBlockButton.js
@@ -5,28 +5,29 @@ import { Given, Then } from "cypress-cucumber-preprocessor/steps";
 Then('the button to download At a Glance should appear at the bottom of the page', () => {
     cy.get(`.ap-button-block`).should('be.visible');
     cy.get(`.ap-button-block a[target='_blank']`).should('be.visible');
-    });
+});
 
-    And('a PDF icon located at {string} should be displayed with a text {string}', (iconUrl,iconText) => {
+And('a PDF icon located at {string} should be displayed with a text {string}', (iconUrl, iconText) => {
     
-        const baseURL = Cypress.config('baseUrl');
-        cy.document().then((document) => {
-            const element = document.querySelector("div[class='ap-button-block'] a");
-            const style = window.getComputedStyle(element, '::before');
-            const image = style.getPropertyValue('background-image');
-            const expectedURL= `url("${baseURL}${iconUrl}")`;
-            expect( expectedURL).equal(image);
-           const text=element.innerText;
-           expect(text).equal(iconText);
-        });
+    const baseURL = Cypress.config('baseUrl');
+    cy.document().then((document) => {
+        const element = document.querySelector("div[class='ap-file-block pdf'] a");
+        const style = window.getComputedStyle(element, '::before');
+        const image = style.getPropertyValue('background-image');
+        const expectedURL = `url("${baseURL}${iconUrl}")`;
+        const newImage = image.replace(/\?v=\w+/, ''); 
+        expect(expectedURL).equal(newImage); 
+        const text = element.innerText;
+        expect(text).equal(iconText);
     });
+});
 
-    And('clicking on the link will open the PDF in a new window', () => {
-        cy.get(`.ap-button-block a`).should('have.attr', "target", "_blank");
-    
-    });
+And('clicking on the link will open the PDF in a new window', () => {
+    cy.get(`.ap-button-block a`).should('have.attr', "target", "_blank");
 
-    When('user clicks on bottom {string} button', (hrefAttr) => {
-        cy.get(`div[class='ap-button-block']`).find('a').trigger('click', {followRedirect:false});
-    
-    });
+});
+
+When('user clicks on bottom {string} button', (hrefAttr) => {
+    cy.get(`div[class='ap-button-block']`).find('a').trigger('click', {followRedirect: false});
+
+});
--- cypress/integration/AnnualPlan/ContentBlockAPLink/contentBlock.js
+++ cypress/integration/AnnualPlan/ContentBlockAPLink/contentBlock.js
@@ -5,19 +5,20 @@ import { Given, Then } from "cypress-cucumber-preprocessor/steps";
 Then('the PDF download link should appear', () => {
     cy.get(`.ap-file-block`).should('be.visible');
     cy.get(`.ap-file-block a[target='_blank']`).should('be.visible');
-    });
+});
 
-And('a PDF icon located at {string} should be displayed with a text {string}', (iconUrl,iconText) => {
+And('a PDF icon located at {string} should be displayed with a text {string}', (iconUrl, iconText) => {
     
     const baseURL = Cypress.config('baseUrl');
     cy.document().then((document) => {
         const element = document.querySelector("div[class='ap-file-block pdf'] a");
         const style = window.getComputedStyle(element, '::before');
         const image = style.getPropertyValue('background-image');
-        const expectedURL= `url("${baseURL}${iconUrl}")`;
-        expect( expectedURL).equal(image);
-       const text=element.innerText;
-       expect(text).equal(iconText);
+        const expectedURL = `url("${baseURL}${iconUrl}")`;
+        const newImage = image.replace(/\?v=\w+/, ''); 
+        expect(expectedURL).equal(newImage); 
+        const text = element.innerText;
+        expect(text).equal(iconText);
     });
 });
 
@@ -31,6 +32,6 @@ Then('the PDF download link is not displayed', () => {
 });
 
 When('user clicks on top {string} button', (hrefAttr) => {
-    cy.get("div[class='ap-file-block pdf']").find('a').trigger('click', {followRedirect:false});
+    cy.get("div[class='ap-file-block pdf']").find('a').trigger('click', {followRedirect: false});
 
 });
