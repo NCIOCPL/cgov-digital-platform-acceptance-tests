44eeaf6fe3f24df5f5bc64a5b91c56d6134c006e
alinai <alinai@users.noreply.github.com>
2022-08-18 15:26:26 -0400

    (#195) Updated Manual Listing Pages Tests

--- cypress/integration/Manual Listing Pages/ManualListingPages/manualListTest.js
+++ cypress/integration/Manual Listing Pages/ManualListingPages/manualListTest.js
@@ -8,12 +8,12 @@ And('the page displays pager info {string}', (pagerInfo) => {
         const newText = text.split('\n').join(' ').split('\t').join('');
         expect(newText).to.match(regex);
     })
-    		
+
 });
 
 
 And('the {int} pager is displayed with the following {string}', (pagerIndex, pagerItems) => {
-	//convert passed string of pager items into an array
+    //convert passed string of pager items into an array
     const expectedPagerItems = pagerItems.split(',');
     let index = 0;
     //only yield visible pager items and verify the count
@@ -26,63 +26,54 @@ And('the {int} pager is displayed with the following {string}', (pagerIndex, pag
 });
 
 And('the page {string} is highlighted', (pageNum) => {
-	cy.get(
-		'.pager__navigation button[class="pager__button active"]:first'
-	).should('have.text', pageNum);
+    cy.get(
+        '.pager__navigation button[class="pager__button active"]:first'
+    ).should('have.text', pageNum);
 });
 
-And('each result displays the trial title with a link in the following format {string}',(linkFormat) => {
-		
-        cy.get('.ct-list-item a').should('have.attr', 'href').then((link) => {
-            expect(link).to.include(linkFormat)
-        })
-	}
+And('each result displays the trial title with a link in the following format {string}', (linkFormat) => {
+
+    cy.get('.ct-list-item a').should('have.attr', 'href').then((link) => {
+        expect(link).to.include(linkFormat)
+    })
+}
 );
 
 And('each result displays the trial summary', () => {
-	cy.get('.ct-list-item p').should('not.be.empty');
+    cy.get('.ct-list-item p').should('not.be.empty');
 });
 
 And('each result displays {string} below the summary', (location) => {
-	cy.get('.ct-list-item .location-info')
-		.should('include.text', location);
+    cy.get('.ct-list-item .location-info')
+        .should('include.text', location);
 });
 
 Then('delighter is displayed with link {string}', (link) => {
-	cy.get('div div.floating-delighter').find('a').should('be.visible').and('have.attr', 'href', link);
+    cy.get('div div.floating-delighter').find('a').should('be.visible').and('have.attr', 'href', link);
 });
 
-When('user clicks on {string} button', (arrow) => {
-	cy.get('.pager__navigation li').contains(arrow).click();
-});
-
-Then('the url has path {string} with query {string}',(path, queryParams) => {
-		cy.location('pathname').should('eq', `${path}/`);
-        cy.location('search').should('eq', queryParams);
-	
-});
 
 When('the user navigates to {string} with non-existent page {string}', (path, parameter) => {
-	cy.visit(`${path}/?${parameter}`);
+    cy.visit(`${path}/?${parameter}`);
 });
 
 Then('the text {string} appears', (text) => {
     cy.get('#NCI-trial-listing-app-root p').should('have.text', text)
 })
 
-When('user clicks on {int} result',(resultNum)=>{
-	cy.get('a.ct-list-item__title')
-		.eq(resultNum - 1)
-		.trigger('click', { followRedirect: false });
+When('user clicks on {int} result', (resultNum) => {
+    cy.get('a.ct-list-item__title')
+        .eq(resultNum - 1)
+        .trigger('click', { followRedirect: false });
 });
 
-And('the pager is not displayed', () => {
-    cy.get('.pager__navigation').should('not.exist');
+Then('user is directed to {string}', (directedUrl) => {
+    cy.visit(directedUrl);
 });
 
 And('intro text {int} paragraph is {string}', (num, introText) => {
-    cy.get('.intro-text').find('p').eq(num - 1).should('have.text',introText);
-    		
+    cy.get('.intro-text').find('p').eq(num - 1).should('have.text', introText);
+
 });
 
 And('{string} link has href {string}', (link, href) => {
