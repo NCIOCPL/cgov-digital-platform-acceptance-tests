653967670e054eeacf786d5ce740d4814202fddc
Kate Mashkina <56886837+kate-mashkina@users.noreply.github.com>
2022-11-10 14:29:50 -0500

    (#277) Added event check for SBIR

--- cypress/e2e/ProductionSmokeTests/$SBIR/productionTests.js
+++ cypress/e2e/ProductionSmokeTests/$SBIR/productionTests.js
@@ -40,3 +40,48 @@ And('page title is {string}', (title) => {
 And('the text {string} is displayed', (str) => {
     cy.get(`p:contains("${str}")`).should('be.visible');
 });
+
+Then('all displayed events have a future date', () => {
+    const months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ];
+    const date = new Date();
+    const currYear = date.getFullYear();
+    const currMonth = date.getMonth();
+    const currDay = date.getDate();
+
+
+    cy.get('div.event-date-time').each(($el) => {
+        const monthAndDay = ($el[0].innerText.split(','))[0];
+        const eventMonth = monthAndDay.split(' ')[0];
+        const eventDay = parseInt(monthAndDay.split(' ')[1]);
+        const eventYear =parseInt(($el[0].innerText.split(','))[1].trim());
+        // get index of month that matches event month
+        const monthOffset = months.findIndex( element => element === eventMonth);
+        //start with verifying the year - if year is less than current fail
+        if (eventYear < currYear) {
+            throw new Error("event displayed is from the past year")
+            // next check month : if the year is current and month is older than current month then fail
+        } else if ((monthOffset < currMonth) && (eventYear == currYear)) {
+            throw new Error(`event displayed is from the previous month/months: ${eventMonth}`)
+            //lastly if year and month passes, check the day - if the month and year are current, fail if its older day
+        } else if (((eventDay) < currDay) && (monthOffset == currMonth) && (eventYear == currYear)) {
+            throw new Error(`event displayed is from the previous day/days: ${monthAndDay}`)
+        } else {
+            //if all above check passes then pass the test
+            expect(true).to.be.true
+        }
+
+    })
+});
