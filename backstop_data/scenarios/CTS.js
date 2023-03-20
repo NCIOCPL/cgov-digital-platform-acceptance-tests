b99228c758cda3bda254e4d4db563f9866ba9f8f
KateMashkinaNIH <mashkinaea@nih.gov>
2023-03-20 11:14:14 -0400

    (#419) Added backstop cts test

---
+++ backstop_data/scenarios/CTS.js
@@ -0,0 +1,10 @@
+module.exports = [
+    {
+        label: 'CTS basic search form',
+        testPath: '/about-cancer/treatment/clinical-trials/search',
+    },
+    {
+        label: 'CTS advanced search form',
+        testPath: '/about-cancer/treatment/clinical-trials/search/advanced',
+    },
+ ]
