11e3bce0ba03f2c2c86442b8eccb5de0e768ec5f
KateMashkinaNIH <mashkinaea@nih.gov>
2023-05-09 14:55:57 -0400

    (#454) usa prose Closes #454

---
+++ backstop_data/scenarios/TextStyles.js
@@ -0,0 +1,17 @@
+module.exports = [
+	{
+		label: 'usa prose english',
+		testPath: '/test/test-usa-prose'
+	},
+    {
+		label: 'usa prose spanish',
+		testPath: '/espanol/test-es/test-usa-prose',
+        viewports: [
+            {
+                label: 'desktop',
+                width: 1025,
+                height: 600,
+            }
+        ]
+	}
+]
