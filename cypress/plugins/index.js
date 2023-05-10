d7d1d6d04f853660ed138b78496fd67d863e58bd
KateMashkinaNIH <mashkinaea@nih.gov>
2023-05-10 14:36:29 -0400

    (#192) Added RSS Feed blog series test - added tests - added fs dependency to remove response folder

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,9 +11,28 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 const cucumber = require('cypress-cucumber-preprocessor').default;
+const { rmdir } = require('fs')
 
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
   on('file:preprocessor', cucumber())
+  on('task', {
+    deleteFolder(folderName) {
+        return new Promise((resolve, reject) => {
+            rmdir(folderName, { maxRetries: 10, recursive: true }, (err) => {
+                if (err) {
+                    console.error(err)
+
+                    return reject(err)
+                }
+
+                resolve(null)
+            })
+        })
+    },
+})
 }
+
+
+
