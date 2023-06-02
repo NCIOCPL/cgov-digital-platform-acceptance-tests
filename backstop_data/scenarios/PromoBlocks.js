14a1e5212bad4d7f49147b87ac8fe1107685062c
KateMashkinaNIH <mashkinaea@nih.gov>
2023-06-02 16:42:57 -0400

    (#476) Backstop and references

---
+++ backstop_data/scenarios/PromoBlocks.js
@@ -0,0 +1,10 @@
+module.exports = [
+    {
+        label: 'NCIDS Internal promo blocks',
+        testPath: '/ncids-internal-promo-block-test',
+    },
+    {
+        label: 'NCIDS External promo blocks',
+        testPath: '/ncids-external-promo-block-test',
+    },
+]
