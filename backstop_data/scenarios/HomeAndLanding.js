b5097b0fae03a1779f283d1a6663e42b38318483
KateMashkinaNIH <mashkinaea@nih.gov>
2023-06-02 17:57:45 -0400

    (#478) Hero and CTA backstop

--- backstop_data/scenarios/HomeAndLanding.js
+++ backstop_data/scenarios/HomeAndLanding.js
@@ -2,5 +2,22 @@ module.exports = [
     {
         label: 'Home and landing with title',
         testPath: '/ncids-landing-with-title',
+    },
+    {
+        label: 'Home and landing Hero and CTA with button',
+        testPath: '/ncids-hero-button-test',
+    },
+    {
+        label: 'Home and landing Default Hero',
+        testPath: '/ncids-hero-default-test',
+    },
+    {
+        label: 'Home and Landing with title CTA',
+        testPath: '/ncids-cta-title-test',
+    },
+    {
+        label: 'Home and Landing without title CTA',
+        testPath: '/ncids-cta-test',
     }
+
 ]
