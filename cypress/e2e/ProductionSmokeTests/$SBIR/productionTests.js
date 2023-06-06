bdfa09f3b3eb75cf08424731e2c6e25ed9ebb928
KateMashkinaNIH <mashkinaea@nih.gov>
2023-06-06 13:10:25 -0400

    (#488) refactored event dates handling

--- cypress/e2e/ProductionSmokeTests/$SBIR/productionTests.js
+++ cypress/e2e/ProductionSmokeTests/$SBIR/productionTests.js
@@ -66,13 +66,23 @@ Then('all displayed events have a future date or text {string} is displayed', (n
     const currDay = date.getDate();
 
     const eventsDateLocator = 'div.event-date-time';
+    let monthAndDay;// this is used to separate Month DD from YYYY such as here: June 8, 2023
+
     cy.document().then((document) => {
         const events = document.querySelector(eventsDateLocator);
         if (events === null) {
             cy.get('div.views-element-container').should('include.text', noEventsText);
         } else {
             cy.get(eventsDateLocator).each(($el) => {
-                const monthAndDay = ($el[0].innerText.split(','))[0];
+                const dateString = $el[0].innerText;
+                if (dateString.includes('|')) { // event date example "June 13, 2023 | 11:00 AM – 3:30 PM", tackling it first to avoid wrong split of case "June 5 – June 8, 2023"
+                    monthAndDay = dateString.split('|')[0].split(',')[0].trim();
+                } else if (dateString.includes('–')) {//event date example "June 5 – June 8, 2023", only care when event ends hence [1]
+                    monthAndDay = dateString.split('–')[1].trim().split(',')[0].trim()
+                } else { // the last event date type case "June 8, 2023"
+                    monthAndDay = $el[0].innerText.split(',')[0].trim();
+                }
+
                 const eventMonth = monthAndDay.split(' ')[0];
                 const eventDay = parseInt(monthAndDay.split(' ')[1]);
                 const eventYear = parseInt(($el[0].innerText.split(','))[1].trim());
