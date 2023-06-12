f30350bd8d09ea4c5625ef18a839782105845954
KateMashkinaNIH <mashkinaea@nih.gov>
2023-06-12 14:13:02 -0400

    (#495) Backstop new home page

--- backstop_data/scenarios/HomePage.js
+++ backstop_data/scenarios/HomePage.js
@@ -1,10 +1,58 @@
 module.exports = [
 	{
-		label: 'home page eng',
-		testPath: '/',
+		label: 'OLD home page eng',
+		testPath: '/cgdpl',
 	},
     {
-		label: 'home page spanish',
+		label: 'OLD home page spanish',
+		testPath: '/espanol/cgdpl',
+	},
+	{
+		label: 'NCIDS home page eng ',
+		testPath: '/',
+		selectors: [
+            'body.has-translated-content'
+        ],
+		viewports: [
+            {
+				label: 'mobile',
+                width: 320,
+                height: 5000,
+            },
+			{
+                label: 'desktop',
+                width: 1025,
+                height: 600,
+            },
+            {
+                label: 'tablet',
+                width: 650,
+                height: 5000,
+            },
+		]
+	},
+    {
+		label: 'NCIDS home page spanish',
 		testPath: '/espanol',
+		selectors: [
+            'body.has-translated-content'
+        ],
+		viewports: [
+            {
+                label: 'desktop',
+                width: 1025,
+                height: 600,
+            },
+            {
+                label: 'tablet',
+                width: 650,
+                height: 5000,
+            },
+			{
+				label: 'mobile',
+                width: 320,
+                height: 5000,
+            }
+		]
 	}
 ]
