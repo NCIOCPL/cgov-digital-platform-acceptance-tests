cce83fc0fb9a2c907522221421102deb3b6a344e
alinai <alinai@users.noreply.github.com>
2023-07-05 09:35:41 -0400

    (#418) Backstop Regression for App Modules tests

---
+++ backstop_data/scenarios/AppModules.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        label: 'CTS basic search form',
+        testPath: '/about-cancer/treatment/clinical-trials/search',
+    },
+    {
+        label: 'CTS advanced search form',
+        testPath: '/about-cancer/treatment/clinical-trials/search/advanced',
+    },
+    {
+        label: 'NCI Search Results - English',
+        testPath: '/search/results?swKeyword=yer',
+    },
+    {
+        label: 'NCI Search Results - Spanish',
+        testPath: '/espanol/buscar/resultados?swKeyword=bon',
+    },
+    {
+        label: 'Dictionary of Cancer Terms Search Form - English No Image',
+        testPath: '/publications/dictionaries/cancer-terms/search/valerian/?searchMode=Begins',
+    },
+    {
+        label: 'Dictionary of Cancer Terms Definition Page - Spanish Image',
+        testPath: '/espanol/publicaciones/diccionarios/diccionario-cancer/def/edr',
+    },
+ ]
