3a6a64d3381d62dd9059bc3867046776dd845f28
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2023-09-06 09:27:40 -0400

    (#549) Ensure all images load

--- backstop_data/engine_scripts/playwright/onReady.js
+++ backstop_data/engine_scripts/playwright/onReady.js
@@ -1,5 +1,17 @@
 module.exports = async (page, scenario, viewport, isReference, browserContext) => {
   console.log('SCENARIO > ' + scenario.label);
+
+  // Attempt to force all images on the page to load.
+  await page.evaluate(async () => {
+    document.querySelectorAll('[loading="lazy"]').forEach((element) => {
+      element.loading = 'eager';
+    });
+
+    document.querySelectorAll('[decoding="async"]').forEach((element) => {
+      element.decoding = 'sync';
+    });
+  });
+
   await require('./clickAndHoverHelper')(page, scenario);
 
   // add more ready handlers here...
