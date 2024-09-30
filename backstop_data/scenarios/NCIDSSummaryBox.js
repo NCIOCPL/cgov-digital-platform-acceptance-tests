8c25175988f058f58b2ac98cf1f0fbc0f71a4644
KateMashkinaNIH <mashkinaea@nih.gov>
2024-09-30 13:16:57 -0400

    (#676) Summary box

---
+++ backstop_data/scenarios/NCIDSSummaryBox.js
@@ -0,0 +1,18 @@
+module.exports = [
+    {
+        label: 'NCIDS summary box',
+        testPath: '/test/ncids-mini-landing-summary-box',
+        viewports: [
+            {
+                label: 'mobile',
+                width: 320,
+                height: 800,
+            },
+            {
+                label: 'desktop',
+                width: 1025,
+                height: 600,
+            }
+        ],
+    },
+]
