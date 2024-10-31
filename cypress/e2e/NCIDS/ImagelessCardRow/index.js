b4a8817b503d24589732bd92cac02e2bff7805a7
KateMashkinaNIH <mashkinaea@nih.gov>
2024-10-31 09:25:14 -0400

    (#695) ACSF changes

--- cypress/e2e/NCIDS/ImagelessCardRow/index.js
+++ cypress/e2e/NCIDS/ImagelessCardRow/index.js
@@ -1,13 +1,20 @@
 /// <reference types="Cypress" />
 import { When } from "cypress-cucumber-preprocessor/steps";
-
+import { getBaseDirectory } from '../../../utils';
 When('the following imageless cards are displayed', (dataTable) => {
     for (let { cardIndex, componentVariant, cardType, linkHref, title, description } of dataTable.hashes()) {
         cy.get(`[class='cgdp-imageless-card-group cgdp-imageless-card-group--${componentVariant}']`)
             .find('.nci-card').eq(cardIndex).as('card')
 
         cy.get('@card').find('a').should('have.attr', 'data-eddl-landing-item-link-type', cardType);
-        cy.get('@card').find('a').should('have.attr', 'href', linkHref)
+
+        cy.get('@card').find('a').invoke('attr', 'href').then(href => {
+            if (href.startsWith('http')) {
+                expect(href).to.eq(linkHref)
+            } else {
+                expect(href).to.eq(`${getBaseDirectory()}${linkHref}`)
+            }
+        })
         cy.get('@card').find('.nci-card__body').find('.nci-card__title').should('have.text', title)
         if (description !== 'none')
             cy.get('@card').find('.nci-card__body').find('.nci-card__description').should('have.text', description)
