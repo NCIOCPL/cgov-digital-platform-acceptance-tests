3459aca6f15ff8116af3a16b76bb8cb96e1bb9fe
KateMashkinaNIH <mashkinaea@nih.gov>
2024-10-31 11:10:32 -0400

    (#696) Backstop tests for NCIDS components

---
+++ backstop_data/scenarios/NCIDSTwoColumn.js
@@ -0,0 +1,111 @@
+module.exports = [
+    {
+        label: 'NCIDS two column 1x1 image',
+        testPath: '/test/ncids-mini-landing-2-column-layout-1x1-image',
+        viewports: [
+            {
+                label: 'desktop',
+                width: 1025,
+                height: 600,
+            },
+            {
+                label: 'tablet',
+                width: 650,
+                height: 900,
+            }
+        ]
+    },
+    {
+        label: 'NCIDS two column 4x3 image',
+        testPath: '/test/ncids-mini-landing-2-column-layout-4x3-image',
+        viewports: [
+            {
+                label: 'desktop',
+                width: 1025,
+                height: 600,
+            },
+            {
+                label: 'mobile',
+                width: 320,
+                height: 800,
+            }
+        ]
+    },
+    {
+        label: 'NCIDS two column 16x9 image',
+        testPath: '/test/ncids-mini-landing-2-column-layout-16x9-image',
+        viewports: [
+            {
+                label: 'tablet',
+                width: 650,
+                height: 900,
+            },
+            {
+                label: 'mobile',
+                width: 320,
+                height: 800,
+            }
+        ]
+    },
+
+    {
+        label: 'NCIDS spanish two column 1x1 image',
+        testPath: '/espanol/test-es/ncids-mini-landing-2-column-layout-1x1-image',
+        viewports: [
+            {
+                label: 'mobile',
+                width: 320,
+                height: 800,
+            }
+        ]
+    },
+    {
+        label: 'NCIDS spanish two column 4x3 image',
+        testPath: '/espanol/test-es/ncids-mini-landing-2-column-layout-4x3-image',
+        viewports: [
+            {
+                label: 'tablet',
+                width: 650,
+                height: 900,
+            }
+        ]
+    },
+    {
+        label: 'NCIDS spanish two column 16x9 image',
+        testPath: '/espanol/test-es/ncids-mini-landing-2-column-layout-16x9-image',
+        viewports: [
+            {
+                label: 'desktop',
+                width: 1025,
+                height: 600,
+            }
+        ]
+    },
+    {
+        label: 'NCIDS two column video',
+        testPath: '/test/ncids-mini-landing-2-column-layout-video',
+        viewports: [
+            {
+                label: 'tablet',
+                width: 650,
+                height: 900,
+            },
+            {
+                label: 'mobile',
+                width: 320,
+                height: 800,
+            }
+        ]
+    },
+    {
+        label: 'NCIDS spanish two column video',
+        testPath: '/espanol/test-es/ncids-mini-landing-2-column-layout-video',
+        viewports: [
+            {
+                label: 'desktop',
+                width: 1025,
+                height: 600,
+            }
+        ]
+    }
+]
