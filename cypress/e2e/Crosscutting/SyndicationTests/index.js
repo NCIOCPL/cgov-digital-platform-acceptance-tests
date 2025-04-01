420b22fe5fe3d5304efd2d89551179e897c2f345
KateMashkinaNIH <mashkinaea@nih.gov>
2025-04-01 12:38:38 -0400

    (#768) NCIDS article footer update

--- cypress/e2e/Crosscutting/SyndicationTests/index.js
+++ cypress/e2e/Crosscutting/SyndicationTests/index.js
@@ -3,19 +3,13 @@ import { And, Then } from 'cypress-cucumber-preprocessor/steps';
 
 
 Then('the syndication text {string} is present', (expectedText) => {
-    cy.get('div.syndication').find('p').should('be.visible').and('have.text', expectedText);
+    cy.get('div.cgdp-hhs-syndication').find('p').should('be.visible').and('have.text', expectedText);
 });
 
 And('the link {string} has a href {string}', (linkText, linkHref) => {
-    cy.get('div.syndication').find('a').should('have.text', linkText).and('have.attr', 'href', linkHref);
+    cy.get('div.cgdp-hhs-syndication').find('a').should('have.text', linkText).and('have.attr', 'href', linkHref);
 });
 
-And('syndication icon located at {string} is present', (iconUrl) => {
-    cy.document().then((document) => {
-        const element = document.querySelector("div.syndication>div");
-        const style = window.getComputedStyle(element);
-        const image = style.getPropertyValue('background-image');
-        const newImage = image.replace(/\?v=\w+/, '');
-        expect(newImage).to.include(iconUrl);
-    });
+And('syndication icon is present', () => {
+    cy.get('div.cgdp-hhs-syndication').find('svg').should('be.visible')
 });
