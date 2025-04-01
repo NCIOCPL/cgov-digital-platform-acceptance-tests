b91812cd257498ff36c2c0e981d44bfe8214ec7a
KateMashkinaNIH <mashkinaea@nih.gov>
2025-04-01 12:38:38 -0400

    (#768) NCIDS article footer update

--- backstop_data/scenarios/BiographyMobile.js
+++ backstop_data/scenarios/BiographyMobile.js
@@ -12,26 +12,26 @@ module.exports = [
         ]
     },
     
-    {
-        label: 'biography expand accordion and then hover over link',
-        testPath: '/about-cancer/coping/feelings/relaxation/loukissas-jen-biography',
-        chainedActionSelectors: [
-            {
-                action: 'click',
-                selector: '.related-resources.list h2',
-            },
-            {
-                action: 'hover',
-                selector: '.related-resources.list a',
-            },
-        ],
-        postInteractionWait: 1000,
-        viewports: [
-            {
-                label: 'mobile',
-                width: 320,
-                height: 800,
-            },
-        ],
-    },
+    // {
+    //     label: 'biography expand accordion and then hover over link',
+    //     testPath: '/about-cancer/coping/feelings/relaxation/loukissas-jen-biography',
+    //     chainedActionSelectors: [
+    //         {
+    //             action: 'click',
+    //             selector: '.cgdp-related-resources.list h2',
+    //         },
+    //         {
+    //             action: 'hover',
+    //             selector: '.cgdp-related-resources.list a',
+    //         },
+    //     ],
+    //     postInteractionWait: 1000,
+    //     viewports: [
+    //         {
+    //             label: 'mobile',
+    //             width: 320,
+    //             height: 800,
+    //         },
+    //     ],
+    // },
 ]
