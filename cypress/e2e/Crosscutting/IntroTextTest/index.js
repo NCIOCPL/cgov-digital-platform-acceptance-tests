6f7bd220081f3edde35d529a0990684cd4a9e876
KateMashkinaNIH <mashkinaea@nih.gov>
2025-05-27 16:12:17 -0400

    (#805) NCIDS intro text changes

--- cypress/e2e/Crosscutting/IntroTextTest/index.js
+++ cypress/e2e/Crosscutting/IntroTextTest/index.js
@@ -2,15 +2,15 @@
 import { Given, And, Then } from 'cypress-cucumber-preprocessor/steps';
 
 Then('intro text section is visible', () => {
-    cy.get('div.blog-intro-text').should('be.visible');
+    cy.get('div[class*="intro-text"]').should('be.visible');
 });
 
 And('intro text section contains translated {string}', (translatedText) => {
-    cy.get('div.blog-intro-text p').should('contain.text', translatedText);
+    cy.get('div[class*="intro-text"] p').should('contain.text', translatedText);
 });
 
 Then('intro text section does not exist', () => {
-    cy.get('div.blog-intro-text').should('not.exist');
+    cy.get('div[class*="intro-text"]').should('not.exist');
 });
 
 
