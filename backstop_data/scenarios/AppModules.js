e4e8fd5ebfba9a13b87fde3d3eec4605119b6497
KateMashkinaNIH <mashkinaea@nih.gov>
2025-08-26 10:25:56 -0400

    Only app modules tests

--- backstop_data/scenarios/AppModules.js
+++ backstop_data/scenarios/AppModules.js
@@ -10,9 +10,25 @@ module.exports = [
     {
         label: 'Dictionary of Cancer Terms Search Form - English No Image',
         testPath: '/publications/dictionaries/cancer-terms/search/valerian/?searchMode=Begins',
+        postInteractionWait:5000
     },
     {
         label: 'Dictionary of Cancer Terms Definition Page - Spanish Image',
         testPath: '/espanol/publicaciones/diccionarios/diccionario-cancer/def/edr',
+          postInteractionWait:5000
     },
+    {
+        label: 'Site wide search english - best bets',
+        testPath: '/search/results?swKeyword=pact',
+    },
+      {
+        label: 'Site wide search english - glossary box',
+        testPath: '/search/results?swKeyword=tumor',
+    },
+    {
+        label: 'Site wide search english - glossary and best bets box',
+        testPath: '/search/results?swKeyword=mantle+cell+lymphoma',
+    },
+
+    
  ]
