f463e61f454d092c5fcba4d751986e3defc9481a
KateMashkinaNIH <mashkinaea@nih.gov>
2025-09-18 16:32:24 -0400

    microsites

---
+++ backstop_data/scenarios/techtransfer_array.js
@@ -0,0 +1,150 @@
+module.exports = [
+  {
+    label: "about",
+    testPath: "/about",
+  },
+  {
+    label: "about careers",
+    testPath: "/about/careers",
+  },
+  {
+    label: "about success-stories halaven",
+    testPath: "/about/success-stories/halaven",
+  },
+  {
+    label: "about success-stories micatu-tissue-arrayer",
+    testPath: "/about/success-stories/micatu-tissue-arrayer",
+  },
+  {
+    label: "about success-stories velcade",
+    testPath: "/about/success-stories/velcade",
+  },
+  {
+    label: "fellowships-training fellowships",
+    testPath: "/fellowships-training/fellowships",
+  },
+  {
+    label: "fellowships-training fellowships business-development-marketing-fellowship",
+    testPath: "/fellowships-training/fellowships/business-development-marketing-fellowship",
+  },
+  {
+    label: "fellowships-training fellowships innovation-fellowship",
+    testPath: "/fellowships-training/fellowships/innovation-fellowship",
+  },
+  {
+    label: "fellowships-training fellowships negotiator-fellowship",
+    testPath: "/fellowships-training/fellowships/negotiator-fellowship",
+  },
+  {
+    label: "fellowships-training training",
+    testPath: "/fellowships-training/training",
+  },
+  {
+    label: "fellowships-training training technology-training-ambassadors-program",
+    testPath: "/fellowships-training/training/technology-training-ambassadors-program",
+  },
+  {
+    label: "fellowships-training training technology-training-university",
+    testPath: "/fellowships-training/training/technology-training-university",
+  },
+  {
+    label: "investigators information-extramural-grantees-contractors",
+    testPath: "/investigators/information-extramural-grantees-contractors",
+  },
+  {
+    label: "investigators information-extramural-grantees-contractors ip-sample-plan-1-strategies-for-universities",
+    testPath: "/investigators/information-extramural-grantees-contractors/ip-sample-plan-1-strategies-for-universities",
+  },
+  {
+    label: "investigators information-extramural-grantees-contractors ip-sample-plan-2-resources-datasets-med-schools",
+    testPath: "/investigators/information-extramural-grantees-contractors/ip-sample-plan-2-resources-datasets-med-schools",
+  },
+  {
+    label: "investigators information-extramural-grantees-contractors ip-sample-plan-3-protection-strategies-institutions",
+    testPath: "/investigators/information-extramural-grantees-contractors/ip-sample-plan-3-protection-strategies-institutions",
+  },
+  {
+    label: "investigators information-extramural-grantees-contractors ip-sample-plan-4-data-tool-sharing-research-institutes",
+    testPath: "/investigators/information-extramural-grantees-contractors/ip-sample-plan-4-data-tool-sharing-research-institutes",
+  },
+  {
+    label: "investigators intellectual-property",
+    testPath: "/investigators/intellectual-property",
+  },
+  {
+    label: "investigators inventions",
+    testPath: "/investigators/inventions",
+  },
+  {
+    label: "investigators inventions employee-invention-report",
+    testPath: "/investigators/inventions/employee-invention-report",
+  },
+  {
+    label: "investigators inventions employee-waivers",
+    testPath: "/investigators/inventions/employee-waivers",
+  },
+  {
+    label: "investigators inventions guide-lab-records",
+    testPath: "/investigators/inventions/guide-lab-records",
+  },
+  {
+    label: "investigators inventions invention-development-program",
+    testPath: "/investigators/inventions/invention-development-program",
+  },
+  {
+    label: "investigators inventions invention-review-process",
+    testPath: "/investigators/inventions/invention-review-process",
+  },
+  {
+    label: "investigators inventions resources",
+    testPath: "/investigators/inventions/resources",
+  },
+  {
+    label: "investigators publications-copyright-trademark",
+    testPath: "/investigators/publications-copyright-trademark",
+  },
+  {
+    label: "investigators research-tools",
+    testPath: "/investigators/research-tools",
+  },
+  {
+    label: "partnering co-development-agreements",
+    testPath: "/partnering/co-development-agreements",
+  },
+  {
+    label: "partnering co-development-agreements-process",
+    testPath: "/partnering/co-development-agreements-process",
+  },
+  {
+    label: "partnering licensing-agreements exclusive-licensing-agreements",
+    testPath: "/partnering/licensing-agreements/exclusive-licensing-agreements",
+  },
+  {
+    label: "partnering licensing-agreements licensing-process-policies",
+    testPath: "/partnering/licensing-agreements/licensing-process-policies",
+  },
+  {
+    label: "partnering licensing-agreements non-exclusive-licensing-agreements",
+    testPath: "/partnering/licensing-agreements/non-exclusive-licensing-agreements",
+  },
+  {
+    label: "partnering licensing-agreements startup-license",
+    testPath: "/partnering/licensing-agreements/startup-license",
+  },
+  {
+    label: "partnering sbir-tech-transfer",
+    testPath: "/partnering/sbir-tech-transfer",
+  },
+  {
+    label: "about sitemap",
+    testPath: "/about/sitemap",
+  },
+  {
+    label: "investigators information-extramural-grantees-contractors ip-sample-plan-5-management-plan-universities",
+    testPath: "/investigators/information-extramural-grantees-contractors/ip-sample-plan-5-management-plan-universities",
+  },
+  {
+    label: "investigators information-extramural-grantees-contractors ip-sample-plan-6-management-plan-objectives-summary",
+    testPath: "/investigators/information-extramural-grantees-contractors/ip-sample-plan-6-management-plan-objectives-summary",
+  },
+];
