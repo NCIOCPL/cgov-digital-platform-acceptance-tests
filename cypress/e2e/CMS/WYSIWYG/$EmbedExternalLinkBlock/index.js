05697269c26c125b0e4402781fa966a2abb1557b
KateMashkinaNIH <mashkinaea@nih.gov>
2025-12-09 11:37:24 -0500

    (#930) NCIDS external link block embed

---
+++ cypress/e2e/CMS/WYSIWYG/$EmbedExternalLinkBlock/index.js
@@ -0,0 +1,17 @@
+/// <reference types="Cypress" />
+import { And } from 'cypress-cucumber-preprocessor/steps';
+
+
+And('user enters {string} into content title search box and clicks {string}', (nameToSearch, applyBtn) => {
+    cy.getIframeBody('iframe#entity_browser_iframe_embedded_external_link_browser').find(`input[id*="edit-info"]`).type(nameToSearch);
+    cy.getIframeBody('iframe#entity_browser_iframe_embedded_external_link_browser').find(`input[id*="edit-submit-block-content-browser"][value=${applyBtn}]`).click({ force: true });
+    cy.wait(2000)
+});
+
+And('user selects {string} item from the content list', (contentTitle) => {
+    cy.getIframeBody('iframe#entity_browser_iframe_embedded_external_link_browser').find('input[name^="entity_browser_select"][type="checkbox"]').check();
+});
+
+And('user clicks on {string} button to select the media', (selectImage) => {
+    cy.getIframeBody('iframe#entity_browser_iframe_embedded_external_link_browser').find(`input[id="edit-submit"][value='${selectImage}']`).click({ force: true });
+});
