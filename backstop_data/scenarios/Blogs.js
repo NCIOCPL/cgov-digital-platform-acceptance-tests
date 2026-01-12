4640519bf700ce62e342b889ae3364687d0feb41
KateMashkinaNIH <mashkinaea@nih.gov>
2026-01-12 14:34:04 -0500

    ref images

--- backstop_data/scenarios/Blogs.js
+++ backstop_data/scenarios/Blogs.js
@@ -19,25 +19,6 @@ module.exports = [
 
     },
     {
-        label: 'blog archive expanded',
-        testPath: '/news-events/cancer-currents-blog/2019/cancer-drugs-natural-products-nci-program',
-        clickSelector: 'h2#archive',
-        postInteractionWait: 1000,
-        viewports: [
-            {
-                label: 'desktop',
-                width: 1025,
-                height: 600,
-            }
-        ],
-        selectors: [
-            '#blog-archive-accordion'
-        ],
-        selectorExpansion: true,
-
-    },
-
-    {
         label: 'blog with recommended content',
         testPath: '/news-events/cancer-currents-blog/2019/pancreatic-cancer-targeting-kras-indirectly',
         viewports: [
